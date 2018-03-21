569093d07d4fbaf206537a5a55903f0e221f413f
Sean McDemrott <seanjmac@hotmail.com>
2018-03-21 16:33:44 -0700

    Add reset values function for forms

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,27 @@
 function Tickets (movie, showing, age) {
-  debugger
+  //debugger
   this.movie = movie;
   this.showing = showing;
   this.age = age;
-}
+};
 
 
 $(document).ready(function() {
+  resetValues();
   $("form#tickets").submit(function(event) {
     event.preventDefault();
-    debugger
+    //debugger
     var movie = $("select#movie").val();
     var showing = $("select#showing").val();
     var age = $("select#age").val();
 
     var tickets = new Tickets(movie, showing, age);
+
+    resetValues();
   });
+  function resetValues() {
+    $("select#movie").val("");
+    $("select#showing").val("");
+    $("select#age").val("")
+  }
 });
