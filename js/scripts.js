076dbdba4089f8bc792bf83ae59a4710cb18b077
Sean McDemrott <seanjmac@hotmail.com>
2018-03-21 16:14:21 -0700

    Add constructor for Tickets

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+function Tickets (movie, showing, age) {
+  debugger
+  this.movie = movie;
+  this.showing = showing;
+  this.age = age;
+}
+
+
 $(document).ready(function() {
   $("form#tickets").submit(function(event) {
     event.preventDefault();
@@ -5,5 +13,7 @@ $(document).ready(function() {
     var movie = $("select#movie").val();
     var showing = $("select#showing").val();
     var age = $("select#age").val();
+
+    var tickets = new Tickets(movie, showing, age);
   });
 });
