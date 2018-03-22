a993042164d1ff2475449192886f7257d01f27ff
Guest User <Guest@Epicodus-9C.local>
2018-03-21 19:43:07 -0700

    messed with jQuery to get ticket prices working

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,44 @@
 function Tickets (movie, showing, age) {
-  //debugger
   this.movie = movie;
   this.showing = showing;
   this.age = age;
 };
 
-// if (this.showing === "12:00pm" || "3:00pm") {
-//    var matPrice = 5
+function ticketPriceMaker(showing, age) {
+  debugger;
+  var ticketPrice = 150;
+  if ((showing !== "6:00pm" || "9:00pm") && (age !== "Adult")) {
+      ticketPrice -= 7;
+    } else {
+      ticketPrice += 0
+    } return ticketPrice
+  }
+// else {
+//       ticketPrice -= 2;
+//     }
+//   } else if (showing === "6:00pm" || "9:00pm") {
+//     if (age === "Youth" || "Senior") {
+//       ticketPrice -= 5;
+//     } else {
+//       return ticketPrice
+//     }
+//   } else {
+//     return ticketPrice
+//   }
+// }
+
+
+//     ticketPrice -= 5;
+//   } else if (age === "Youth" || "Senior") {
+//     ticketPrice -= 2;
+//   } else {
+//     ticketPrice = ticketPrice + 0;
+//   // } else if (age === "Adult") {
+//   //   ticketPrice = ticketPrice + 0;
+//   } return ticketPrice;
+// }
+
+
 function resetValues() {
   $("select#movie").val("");
   $("select#showing").val("");
@@ -23,6 +55,11 @@ $(document).ready(function() {
     var age = $("select#age").val();
 
     var tickets = new Tickets(movie, showing, age);
+    var ticketPrice = ticketPriceMaker(showing, age)
+
+    $("#movie-name").text(tickets.movie);
+    $("#movie-showing").text(tickets.showing);
+    $("#movie-price").text(ticketPrice);
 
     $("#user-result").show();
 
